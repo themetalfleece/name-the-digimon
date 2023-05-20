bd2f991ee6158390d46f8476b609993d87045818
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-20 14:54:31 +0300

    add full functionality to getObscurifiedName

--- src/features/obsurifiedName/getObscurifiedName.util.ts
+++ src/features/obsurifiedName/getObscurifiedName.util.ts
@@ -1,8 +1,30 @@
 import { ObscurifiedName } from "./obscurifiedName.type";
 
 export const getObscurifiedName = (name: string): ObscurifiedName => {
-  return name.split("").map((letter) => ({
+  // reveal all but letters
+  const obscurifiedName = name.split("").map((letter) => ({
     letter,
-    isRevealed: Math.random() > 0.5,
+    isRevealed: !/[A-Za-z]/.test(letter),
   }));
+
+  // reveal "mon" in the end of the Digimon name
+  for (let i = 0; i < obscurifiedName.length; i++) {
+    const letter = obscurifiedName[i];
+    const nextLetter = obscurifiedName[i + 1];
+    const nextNextLetter = obscurifiedName[i + 2];
+    const finalLetter = obscurifiedName[i + 3];
+
+    if (
+      letter.letter === "m" &&
+      nextLetter.letter === "o" &&
+      nextNextLetter.letter === "n" &&
+      (!finalLetter || finalLetter.letter === " ")
+    ) {
+      letter.isRevealed = true;
+      nextLetter.isRevealed = true;
+      nextNextLetter.isRevealed = true;
+    }
+  }
+
+  return obscurifiedName;
 };
