3586e91f332327bf613e4b4e2206d8e3fd37ce44
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-20 14:38:58 +0300

    add various placeholder info to Game

--- src/components/Game/Game.component.tsx
+++ src/components/Game/Game.component.tsx
@@ -1,10 +1,28 @@
-import { Component, JSXElement } from "solid-js";
-import { Header } from "./Game.styles";
+import { Component, For, JSXElement } from "solid-js";
+import { Container, DigimonAvatar, Header, NameContainer } from "./Game.styles";
 
 export const Game: Component = () => {
+  const imageUrl = "https://digimon-api.com/images/digimon/w/Garurumon.png";
+  const digimonName = "Beelzemon (X Antibody)";
+  const digimonNameData = digimonName.split("").map((letter) => ({
+    letter,
+    isRevealed: Math.random() > 0.5,
+  }));
+
+  console.log(digimonNameData);
+
   return (
-    <div>
+    <Container>
       <Header>Name the Digimon</Header>
-    </div>
+      <DigimonAvatar src={imageUrl} alt="Image of the Digimon to find" />
+
+      <NameContainer>
+        <For each={digimonNameData}>
+          {({ letter, isRevealed }): JSXElement => (
+            <div>{isRevealed ? letter : "_"}</div>
+          )}
+        </For>
+      </NameContainer>
+    </Container>
   );
 };
--- src/components/Game/Game.styles.ts
+++ src/components/Game/Game.styles.ts
@@ -1,5 +1,25 @@
 import { styled } from "solid-styled-components";
 
+export const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
 export const Header = styled("h1")`
   color: ${({ theme }) => theme!.colors.info};
+`;
+
+export const DigimonAvatar = styled("img")`
+  border-radius: 18px;
+  max-width: 100%;
+`;
+
+export const NameContainer = styled("div")`
+  display: flex;
+  gap: 8px;
+  font-size: 32px;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
