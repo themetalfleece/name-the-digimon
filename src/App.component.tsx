cb34ba1c20d781ce2047b48b12616685c00fd50b
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-20 19:03:02 +0300

    add ProgressProvider

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -3,15 +3,18 @@ import { Layout } from './lib/Layout/Layout.component';
 import { ThemeProvider } from './lib/ThemeProvider/ThemeProvider.component';
 import { Game } from './components/Game/Game.component';
 import { RoundProvider } from './features/round/round.store';
+import { ProgressProvider } from './features/progress/progress.store';
 
 export const App: Component = () => {
   return (
     <ThemeProvider>
-      <RoundProvider>
-        <Layout>
-          <Game />
-        </Layout>
-      </RoundProvider>
+      <ProgressProvider>
+        <RoundProvider>
+          <Layout>
+            <Game />
+          </Layout>
+        </RoundProvider>
+      </ProgressProvider>
     </ThemeProvider>
   );
 };
