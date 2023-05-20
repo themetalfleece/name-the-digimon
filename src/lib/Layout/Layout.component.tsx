2ad11598563c4ffbab5c99a82387337ab300defa
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-20 20:08:58 +0300

    add copyright mark

--- src/lib/Layout/Layout.component.tsx
+++ src/lib/Layout/Layout.component.tsx
@@ -1,10 +1,20 @@
 import type { Component, JSXElement } from 'solid-js';
 import { Container } from './Layout.styles';
+import { Text } from '../Text/Text.component';
 
 export interface LayoutProps {
   children: JSXElement;
 }
 
 export const Layout: Component<LayoutProps> = props => {
-  return <Container>{props.children}</Container>;
+  return (
+    <Container>
+      {props.children}
+      <Text style={{ 'margin-top': '80px' }}>
+        This app is not affiliated with or endorsed by Bandai or the Digimon
+        franchise. All trademarks and copyrights belong to their respective
+        owners.
+      </Text>
+    </Container>
+  );
 };
