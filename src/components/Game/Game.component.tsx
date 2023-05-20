040401cf4774e8c57798630c4b7dc911732bb045
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-20 15:23:18 +0300

    use round store in Game

--- src/components/Game/Game.component.tsx
+++ src/components/Game/Game.component.tsx
@@ -1,24 +1,28 @@
-import { Component, For, JSXElement } from "solid-js";
+import { Component, For, JSXElement, Show } from "solid-js";
 import { Container, DigimonAvatar, Header, NameContainer } from "./Game.styles";
 import { getObscurifiedName } from "../../features/obsurifiedName/getObscurifiedName.util";
+import { useRound } from "../../features/round/round.store";
 
 export const Game: Component = () => {
-  const imageUrl = "https://digimon-api.com/images/digimon/w/Garurumon.png";
-  const digimonName = "Beelzemon (X Antibody)";
-  const obscurifiedName = getObscurifiedName(digimonName);
+  const { round } = useRound();
 
   return (
     <Container>
       <Header>Name the Digimon</Header>
-      <DigimonAvatar src={imageUrl} alt="Image of the Digimon to find" />
+      <Show when={round.digimon} fallback={<h2>Loading...</h2>}>
+        <DigimonAvatar
+          src={round.digimon?.imageUrl}
+          alt="Image of the Digimon to find"
+        />
 
-      <NameContainer>
-        <For each={obscurifiedName}>
-          {({ letter, isRevealed }): JSXElement => (
-            <div>{isRevealed ? letter : "_"}</div>
-          )}
-        </For>
-      </NameContainer>
+        <NameContainer>
+          <For each={round.obscurifiedName}>
+            {({ letter, isRevealed }): JSXElement => (
+              <div>{isRevealed ? letter : "_"}</div>
+            )}
+          </For>
+        </NameContainer>
+      </Show>
     </Container>
   );
 };
