32d4350e98ca1767b24adf7df7b6afff04bc84fe
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-20 19:13:15 +0300

    add ignoreIds

--- src/features/digimon/getRandomDigimonId.util.ts
+++ src/features/digimon/getRandomDigimonId.util.ts
@@ -1,5 +1,19 @@
 import { maxId, minId } from './digimonStats.constants';
 
-export const getRandomDigimonId = () => {
-  return Math.floor(Math.random() * maxId) + minId;
+export const getRandomDigimonId = (ignoreIds: number[] = []) => {
+  const ignoredSorted = ignoreIds.concat().sort(function (a, b) {
+    return a - b;
+  });
+
+  const logicalMax = maxId - ignoredSorted.length;
+  let randomNumber =
+    Math.floor(Math.random() * (logicalMax - minId + 1)) + minId;
+
+  for (let i = 0; i < ignoredSorted.length; i++) {
+    if (randomNumber >= ignoredSorted[i]) {
+      randomNumber++;
+    }
+  }
+
+  return randomNumber;
 };
