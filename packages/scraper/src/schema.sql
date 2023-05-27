0846243979b98396d7779f9db6669107d9b3e524
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-27 20:52:27 +0300

    Add CF workers scraper (#2)

---
+++ packages/scraper/src/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS digimon (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  wiki_id INTEGER NOT NULL,
+  data TEXT NOT NULL,
+  raw_data TEXT NOT NULL,
+  image_id TEXT NOT NULL,
+  created_at TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_digimon_wiki_id ON digimon(wiki_id);
