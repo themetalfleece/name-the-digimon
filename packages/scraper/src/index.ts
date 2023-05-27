f6cfd2db4bbbccb9b6188ac774613c1dfba8a590
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-05-27 18:13:24 +0300

    init scraper project

---
+++ packages/scraper/src/index.ts
@@ -0,0 +1,11 @@
+export interface Env {}
+
+export default {
+  async scheduled(
+    controller: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<void> {
+    console.log(`Hello World!`);
+  },
+};
