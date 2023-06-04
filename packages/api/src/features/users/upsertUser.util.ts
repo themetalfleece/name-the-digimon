d49b38b900be3566b706c125502af8365159bceb
Jason Athanasoglou <jathanasoglou@outlook.com>
2023-06-04 20:32:39 +0300

    add User type

--- packages/api/src/features/users/upsertUser.util.ts
+++ packages/api/src/features/users/upsertUser.util.ts
@@ -1,31 +1,31 @@
 import { TRPCError } from '@trpc/server';
+import { User } from './user.type';
 
 export const upsertUser = async (DB: D1Database, accessToken: string) => {
   const userRes = await DB.prepare(`SELECT * FROM users WHERE access_token = ?`)
     .bind(accessToken)
-    .all();
+    .all<User>();
 
   const user = userRes.results?.[0];
 
-  if (!user) {
-    const insertResult = await DB.prepare(
-      `INSERT INTO users (access_token, created_at) VALUES (?, ?) RETURNING id, access_token`,
-    )
-      .bind(accessToken, new Date().toISOString())
-      .all<{ id: string; access_token: string }>();
-
-    const createdUser = insertResult.results?.[0];
-
-    if (!createdUser) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Failed to create user',
-      });
-    }
-
-    return {
-      id: createdUser.id,
-      accessToken: createdUser.access_token,
-    };
+  if (user) {
+    return user;
   }
+
+  const insertResult = await DB.prepare(
+    `INSERT INTO users (access_token, created_at) VALUES (?, ?) RETURNING id, access_token`,
+  )
+    .bind(accessToken, new Date().toISOString())
+    .all<User>();
+
+  const createdUser = insertResult.results?.[0];
+
+  if (!createdUser) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Failed to create user',
+    });
+  }
+
+  return createdUser;
 };
---
+++ packages/api/src/features/users/user.type.ts
@@ -0,0 +1,5 @@
+export interface User {
+  id: number;
+  access_token: string;
+  created_at: string;
+}
